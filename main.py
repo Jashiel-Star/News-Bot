0e15f9101bc95493be21d77fe857a3051b8c57a1
Alacompapa <55757920+Alacompapa@users.noreply.github.com>
2022-09-12 10:18:06 +0200

    added logic to show date

--- main.py
+++ main.py
@@ -15,9 +15,11 @@ for url in url_list:
     text = response.text
     html_data = BeautifulSoup(text, 'html.parser')
     #Define the columns
-    header = ["Titles", "Links", "Author"]
+    header = ["Titles", "Links", "Author", "Date"]
+    space = [" ", " ", " ", " "]
     titles_together = ''
     links_together = ''
+    dates_together = ''
     #extracting the data we wish to store
     for a in html_data.find_all('a', class_="post-preview-title newsletter"):
         titles_together += str(a.contents)
@@ -25,22 +27,31 @@ for url in url_list:
         links_together += str(a['href'])
         links_together += ' '
     author = html_data.find('a', class_="navbar-title-link").contents
-    #formatting the titles
+    for time in html_data.find_all('time'):
+        dates_together += str(time.contents)
+        dates_together += ' '
+    #formatting the entries
     for bracket in titles_together:
         titles_together = titles_together.replace('[','')
+        dates_together = dates_together.replace('[','')
+        dates_together = dates_together.replace(']','')
     #separating each title and link into individuals
     titles = titles_together.split("] ")
     links = links_together.split(" ")
+    dates = dates_together.split(" '")
     authors = [None]*(len(titles)-1)
     for index in range(len(titles) - 1):
         authors[index] = author
     final_news = []
     if len(titles) == len(links):
-        final_news = list(zip(titles, links, authors))
+        final_news = list(zip(titles, links, authors, dates))
     else:
         raise Exception('Missing titles or links')
     #write the data into the file
     with open("articles.csv", 'a', encoding="utf-8", newline='') as open_file:
         writer = csv.writer(open_file)
+        writer.writerow(space)
+        writer.writerow(header)
         writer.writerows(final_news)
         open_file.close()
+
